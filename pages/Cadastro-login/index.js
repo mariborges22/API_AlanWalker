327e21493e397a3987a0efc16f3ec21a01fb9528
Bruno <110209076+Bruno-al@users.noreply.github.com>
2023-07-01 23:36:58 -0300

    Update index.js

--- pages/Cadastro-login/index.js
+++ pages/Cadastro-login/index.js
@@ -1,8 +1,8 @@
 function irParaOutraPagina() {
-    window.location.href = "/pages/principal/main-page.html";
+    window.location.href = "pages/principal/main-page.html";
   }
 function voltar() {
 
-    window.location.href = "/API_AlanWalker/pages/main-page.html";
+    window.location.href = "API_AlanWalker/pages/main-page.html";
   }
 
