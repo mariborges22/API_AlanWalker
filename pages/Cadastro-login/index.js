3948f4de2110662b1294d9bb28132d2d9ea12f43
Bruno <brunoassis2029@gmail.com>
2023-07-01 23:24:54 -0300

    iniciando pagina princial

--- pages/Cadastro-login/index.js
+++ pages/Cadastro-login/index.js
@@ -2,5 +2,5 @@ function irParaOutraPagina() {
     window.location.href = "../pages/principal/main-page.html";
   }
 function voltar() {
-    window.location.href = "../pages/main-page.html";
+    window.location.href = "../../../index.html";
   }
